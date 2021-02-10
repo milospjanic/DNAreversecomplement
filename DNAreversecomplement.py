d36fe113540ba10315ec4232e272592629020a92
Milos Pjanic <pjanic.milos@gmail.com>
2021-02-10 08:19:07 -0500

    Create  DNAreversecomplement.py

---
+++ DNAreversecomplement.py
@@ -0,0 +1,10 @@
+def complement(s): 
+    basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+    letters = list(s) 
+    letters = [basecomplement[base] for base in letters] 
+    return ''.join(letters)
+def revcom(s):
+    return complement(s[::-1])
+print("ACGTAAA")
+print(complement("ACGTAAA"[::-1]))
+print(revcom("ACGTAAA"))
